9ad654320a105fe31c9b7bb623e1aa1e253e3c72
frontend200tb <frontend200tb@mail.ru>
2024-02-24 19:35:18 +0300

    feat: Rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -180,12 +180,15 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  this.width = width;
-  this.height = height;
-  this.getArea = function () {
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
     return this.width * this.height;
-  };
+  }
 }
 
 /**
